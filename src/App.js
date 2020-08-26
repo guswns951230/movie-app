4a4c5b61e0bb70bf3676fbb2a730725034b4231a
강현준 <kanghj@ganghyeonjun-ui-MacBookPro.local>
2020-08-26 22:23:47 +0900

    03-4 리액트 기초 개념: props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-function Potato() {
-  return <h1>I like potato</h1>;
+// function Food(props) {
+//   console.log(props);
+//   return <h1>I like {props.fav}</h1>;
+// }
+
+function Food({ fav }) {
+  console.log(1);
+  return <h1>I lIke {fav}</h1>;
 }
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div >
   );
 }
