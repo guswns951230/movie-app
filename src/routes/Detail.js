b47443a978b93cb1ea0aca8385bef179ef56c79e
강현준 <kanghj@ganghyeonjun-ui-MacBookPro.local>
2020-08-27 22:42:44 +0900

    08-5 Movie_App redirect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import './Detail.css';
 
-function Detail(props) {
-  console.log(props);
-  return <span>hello</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push('/');
+    }
+  }
+
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return (
+        <div className="detail-data">
+          <h3>{location.state.title}</h3>
+          <h5>{location.state.year}</h5>
+          <ul>
+            <li>{location.state.genres}</li>
+          </ul>
+          <p>{location.state.summary}</p>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
