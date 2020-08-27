e225254206253f66e9456aaa1494e54620fcfa1c
강현준 <kanghj@ganghyeonjun-ui-MacBookPro.local>
2020-08-27 12:14:41 +0900

    05-4 Movie_app isLoading state

--- src/App.js
+++ src/App.js
@@ -2,50 +2,21 @@ import React from 'react';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    console.log('hello');
-  }
-
   state = {
-    count: 0,
-  };
-
-  add = () => {
-    console.log(1);
-    this.setState(current => ({
-      count: current.count + 1
-    }));
-  };
-
-  minus = () => {
-    console.log('-');
-    this.setState(current => ({
-      count: current.count - 1
-    }));
+    isLoading: true,
+    movies: [],
   };
 
   componentDidMount() {
-    console.log('component rendering');
-  }
-
-  componentDidUpdate() {
-    console.log('just update');
-  }
-
-  componentWillUnmount() {
-    console.log('Goodbye, cruel world');
+    // 영화 데이터 로딩
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
 
   render() {
-    console.log('rendering');
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
 }
 
