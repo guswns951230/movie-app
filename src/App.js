135fc36f9e3d32deb3408551ca6779cfe01e2e42
Kang <rkdguswns0803@gmail.com>
2022-07-07 23:31:39 +0900

    modified

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Home from './routes/Home';
 import Detail from './routes/Detail';
 
 import {
+  BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route path={`/movie/:id`} element={<Detail />} />
-      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-    </Routes>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 import './styles.css';
@@ -8,9 +7,7 @@ import './styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL} >
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
