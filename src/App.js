38e0e170c07b564575492621c8a9656aacae9d26
Kang <rkdguswns0803@gmail.com>
2022-07-07 21:45:03 +0900

    modified App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,19 @@ import Detail from './routes/Detail';
 
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
 } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/movie_app/:id'>
-          <Detail />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
+    <Router basename={process.env.PUBLIC_URL} >
+      <div>
+        <Routes>
+          <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
+          <Route path={`${process.env.PUBLIC_URL}`} element={<Home />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
