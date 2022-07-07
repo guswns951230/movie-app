2681404f50da93f427e221c713fe148ead7cbe9b
Kang <rkdguswns0803@gmail.com>
2022-07-08 02:58:47 +0900

    Movie app

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ import {
 } from 'react-router-dom';
 
 function App() {
+  console.log(process.env);
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router >
       <Routes>
-        <Route path={process.env.PUBLIC_URL} element={<Home />} />
-        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/movie/:id' element={<Detail />} />
       </Routes>
     </Router>
   );
