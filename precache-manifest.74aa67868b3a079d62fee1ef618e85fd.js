ab561f32af8453e3f45f616ae410825c18501fcb
Kang <rkdguswns0803@gmail.com>
2022-07-08 00:34:02 +0900

    Updates

---
+++ precache-manifest.74aa67868b3a079d62fee1ef618e85fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b83c30d1267d56a08184e3161644a57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe6562c6489b8f410a40",
+    "url": "/static/css/main.2363c1c3.chunk.css"
+  },
+  {
+    "revision": "2696ac88d913371919ec",
+    "url": "/static/js/2.b32dac97.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/static/js/2.b32dac97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6562c6489b8f410a40",
+    "url": "/static/js/main.3e30d1e1.chunk.js"
+  },
+  {
+    "revision": "cf27343b5d386281fb60",
+    "url": "/static/js/runtime-main.1e9c0366.js"
+  }
+]);
